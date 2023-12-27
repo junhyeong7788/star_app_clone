b9a87e144172a264e43cbd2275c16323bf7ddb5a
junhyeong7788 <ryker77@naver.com>
2023-12-28 00:02:56 +0900

    bottomNavigationBar Icon add

---
+++ lib/body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class InstaBody extends StatelessWidget {
+  final int index;
+
+  const InstaBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:insta_clone_application/body.dart';
 
 void main() {
   runApp(const InstaCloneApp());
@@ -17,6 +18,11 @@ class InstaCloneApp extends StatelessWidget {
           primary: Colors.white,
           secondary: Colors.black,
         ),
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          showSelectedLabels: false, //바텀 아이콘 라벨 안보이게 해줌
+          showUnselectedLabels: false,
+          selectedItemColor: Colors.black,
+        ),
         useMaterial3: true,
       ),
       home: const InstaCloneHome(),
@@ -24,8 +30,21 @@ class InstaCloneApp extends StatelessWidget {
   }
 }
 
-class InstaCloneHome extends StatelessWidget {
+class InstaCloneHome extends StatefulWidget {
   const InstaCloneHome({super.key});
+
+  @override
+  State<InstaCloneHome> createState() => _InstaCloneHomeState();
+}
+
+class _InstaCloneHomeState extends State<InstaCloneHome> {
+  late int index;
+
+  @override
+  void initState() {
+    super.initState();
+    index = 0;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -58,7 +77,21 @@ class InstaCloneHome extends StatelessWidget {
         ],
       ),
       //appbar는 별도의 위젯으로 들어가있다. 그래서 분리하기 까다로움
-      body: const Placeholder(),
+      body: InstaBody(index: index),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: index, //어떤 인덱스가 들어가있는지 확인하기 위해 사용
+        onTap: (newIndex) => setState(() => index = newIndex),
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+                size: 28,
+              ),
+              label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search, size: 28), label: 'Search')
+        ],
+      ),
     );
   }
 }
